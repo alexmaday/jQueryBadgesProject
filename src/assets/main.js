9dc4283416f1a0c4ef43fcbbfd2e67e8739c302c
Alex Maday <alex.maday@gmail.com>
2017-10-15 19:12:40 -0500

    ajax request for report card

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
-$(function() {
-
+$(document).ready(function () {
   // your code will go here
-
+  let url = 'https://www.codeschool.com/users/amaday68.json';
+  
+  $.ajax(url, {
+      dataType: 'jsonp',
+      success: function (response) {
+        var courses = response.courses.completed;
+        
+        for (var i = 0, l = courses.length; i < l; i++) {
+          $('#badges').append('<p>' + courses[i].title + '</p>');
+          
+        }
+      }
+  });
 });
